30a8680b4db5013b0ebfa1804270e730c80fa656
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-10-13 22:28:08 +0500

    front: added header design

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,9 +1,11 @@
 import './Chat.css';
 import ChatFooter from './ChatFooter/ChatFooter';
+import ChatHeader from './ChatHeader/ChatHeader';
 
 export default function Chat() {
     return (
         <div className="chat-wrapper">
+            <ChatHeader/>
             <ChatFooter/>
         </div>
     );
--- frontend/src/components/Chat/ChatHeader/ChatHeader.tsx
+++ frontend/src/components/Chat/ChatHeader/ChatHeader.tsx
@@ -1,7 +1,10 @@
+import SearchIcon from "../../partials/icons/SearchIcon";
+import './ChatHeader.css';
+
 export default function ChatHeader() {
     return (
         <div className="chat-header-wrapper">
-            
+            <button className="search-btn"><SearchIcon className="search-icon"/></button>
         </div>
     );
 }
