f15b30edf89e00a9261ace13be1a20ced062e03d
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-10-13 22:52:11 +0500

    front: added messages styles

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,11 +1,13 @@
 import './Chat.css';
 import ChatFooter from './ChatFooter/ChatFooter';
 import ChatHeader from './ChatHeader/ChatHeader';
+import MessageBox from './MessageBox/MessageBox';
 
 export default function Chat() {
     return (
         <div className="chat-wrapper">
             <ChatHeader/>
+            <MessageBox/>
             <ChatFooter/>
         </div>
     );
---
+++ frontend/src/components/Chat/MessageBox/MessageBox.tsx
@@ -0,0 +1,12 @@
+import './MessageBox.css';
+
+export default function MessageBox() {
+    return (
+        <div className="message-box-wrapper">
+            <div className="message current">Hello!</div>
+            <div className="message other">Hello to you too!</div>
+            <div className="message current">How are you?</div>
+            <div className="message other">Im great, thanks! You?</div>
+        </div>
+    );
+}
