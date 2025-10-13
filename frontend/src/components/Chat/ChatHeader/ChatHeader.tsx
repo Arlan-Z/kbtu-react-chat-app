ca3f4db642d346c0a8e68e9c7d66d16fb7a34f34
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-10-13 22:40:28 +0500

    front: added header input animations

--- frontend/src/components/Chat/ChatHeader/ChatHeader.tsx
+++ frontend/src/components/Chat/ChatHeader/ChatHeader.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import SearchIcon from "../../partials/icons/SearchIcon";
-import './ChatHeader.css';
+import "./ChatHeader.css";
 
 export default function ChatHeader() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className="chat-header-wrapper">
-            <button className="search-btn"><SearchIcon className="search-icon"/></button>
+            <div className={`search-container ${isOpen ? "open" : ""}`}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="search-input"
+                />
+
+                <button
+                    className="search-btn"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <SearchIcon className="search-icon" />
+                </button>
+            </div>
         </div>
     );
 }
